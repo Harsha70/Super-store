533b61f5050d62bbea7ddaadc277ad9511ef0d02
harsha <manyamharsha112@gmail.com>
2021-04-10 09:49:14 +0530

    added responsiveness to cartitems

--- src/components/CartPage/CartPage.jsx
+++ src/components/CartPage/CartPage.jsx
@@ -8,13 +8,13 @@ import { connect } from 'react-redux';
 
 const CartPage = ({cartItems, price}) => {
     return (
-        <div style={{width: "50%",display: "inline-block"}}>
+        <div className="cartpagewrapper">
             <h1 style={{textAlign:"initial", padding:"15px 0px"}}>Shopping Cart</h1>
             {cartItems.map((cartitem, index)=>(
                 <CartPageItems key={index} cartitem={cartitem}/>
             ))}
             <div style={{display:"flex",flexFlow: "row",justifyContent: "space-between", padding:"10px"}}>
-            <Link to="/checkout"><span class="checkout">Checkout</span></Link>
+            <Link to="/checkout"><span className="checkout">Checkout</span></Link>
                 <span>Total: {price}</span>
             </div>
         </div>
